4d8332e60282ad6c2dea43414ce94d84670e41c7
Sarvesh Borole <101782944+Sarvesh788@users.noreply.github.com>
2024-08-19 11:21:22 +0530

    Update PostRepository.java
    Commented the code.

--- demo1/src/main/java/com/example/demo/Repository/PostRepository.java
+++ demo1/src/main/java/com/example/demo/Repository/PostRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
-@Repository
+@Repository // it makes this as a repository, making it spring-responsible, it also contains built-in methods like findAll and findBy, etc.
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    List<Post> findAll(Sort sort);
+    List<Post> findAll(Sort sort); // it is built in
 }
